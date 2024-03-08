36111f86f2bf9c182f154241f10ce8de2a76eb5f
刘誉 <22371360@buaa.edu.cn>
2024-03-08 19:47:18 +0800

    上传新文件

---
+++ src/expr/FuncFactor.java
@@ -0,0 +1,36 @@
+package expr;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FuncFactor implements Factor {
+    private String newFunc; // 将函数实参带入形参位置后的结果(字符串形式)
+    private Expr expr; // 将newFunc解析成表达式后的结果
+
+    public FuncFactor(String name, ArrayList<Factor> actualParas) {
+        this.newFunc = Definer.callFunc(name, actualParas);
+        this.expr = setExpr();
+    }
+
+    public Expr setExpr() {
+        String str = Adjust.simplifyString(newFunc);
+        Lexer lexer = new Lexer(str);
+        Parser parser = new Parser(lexer);
+        return parser.parseExpr();
+    }
+
+    @Override
+    public void setExp(String exp) {
+
+    }
+
+    @Override
+    public Poly toPoly() throws IOException, ClassNotFoundException {
+        return expr.toPoly();
+    }
+
+    @Override
+    public String toString() {
+        return newFunc;
+    }
+}
