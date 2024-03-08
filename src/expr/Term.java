50a166e3a5e05bdc8917e1c67424e979910a28e7
刘誉 <22371360@buaa.edu.cn>
2024-03-08 19:48:53 +0800

    上传新文件

---
+++ src/expr/Term.java
@@ -0,0 +1,67 @@
+package expr;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Term implements Factor {
+    private String exp = "0";
+    private BigInteger coe = new BigInteger("1"); // Term 的符号, -1 -> -   |   +1 -> +
+    private final HashSet<Factor> factors;
+
+    public Term(int sign) {
+        if (sign == -1) {
+            this.coe = this.coe.negate();
+        }
+        factors = new HashSet<>();
+    }
+
+    public void addFactor(Factor factor) {
+        this.factors.add(factor);
+    }
+
+    public void negate() {
+        coe = coe.negate();
+    }
+
+    public void merge() { // 合并有符号常数并更新 Term 的符号
+        Iterator<Factor> iterator = factors.iterator();
+        while (iterator.hasNext()) {
+            Factor factor = iterator.next();
+            if (factor instanceof Num) {
+                BigInteger num = new BigInteger(((Num) factor).getNum());
+                this.coe = this.coe.multiply(num);
+                iterator.remove();
+            }
+        }
+    }
+
+    @Override
+    public Poly toPoly() throws IOException, ClassNotFoundException {
+        Poly poly = new Poly();
+        poly = poly.monoToPoly(coe, 0);
+        for (Factor factor : factors) {
+            poly = poly.mulPoly(factor.toPoly());
+        }
+        return poly;
+    }
+
+    @Override
+    public void setExp(String exp) {
+        this.exp = exp;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(coe);
+        for (Factor factor : factors) {
+            sb.append("*(");
+            sb.append(factor.toString());
+            sb.append(")");
+        }
+        return sb.toString();
+    }
+
+}
