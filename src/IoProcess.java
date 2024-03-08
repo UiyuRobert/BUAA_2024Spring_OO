bcd037300017fd7a2d295ac0fbf5cf8795c8acd6
刘誉 <22371360@buaa.edu.cn>
2024-03-08 19:45:26 +0800

    上传新文件

---
+++ src/IoProcess.java
@@ -0,0 +1,22 @@
+import expr.Adjust;
+import expr.Definer;
+
+import java.util.Scanner;
+
+public class IoProcess {
+    private Scanner scanner;
+    private String inputInfo;
+
+    public IoProcess(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void processFunction() { // 处理自定义函数
+        int funcNum = scanner.nextInt();
+        scanner.nextLine(); // \n
+        for (int i = 0; i < funcNum; ++i) {
+            String funcInput = Adjust.simplifyString(scanner.nextLine());
+            Definer.addFunc(funcInput);
+        }
+    }
+}
